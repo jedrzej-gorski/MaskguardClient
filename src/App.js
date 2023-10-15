bb53808a8b3319990011da36b3b09fbb3223e620
Jan Majchrzak <jan.majchrzak@protonmail.com>
2023-10-15 12:22:01 +0200

    Swap file selector for a webcam widget

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { QRCodeSVG } from 'qrcode.react'
+import Webcam from 'react-webcam'
 
 
 function App() {
   const [token, setToken] = useState(null);
   const [expirationDate, setExpirationDate] = useState(null);
+  const [imgSrc, setImgSrc] = useState(null);
+  const webcamRef = useRef(null);
 
-  const submitFile = async e => {
-    e.preventDefault()
-
-    const formData = new FormData(e.target);
+  const submitImage = async base64img => {
+    const payload = JSON.stringify({
+      image: base64img
+    })
     const response = await fetch("http://localhost:5001/predict", {
       method: 'post',
-      body: formData
-    });
+      body: payload,
+      headers: { "Content-Type": "application/json" }
+    })
     const json = await response.json();
     console.log(json);
     if (json.token) {
       localStorage.setItem("token", json.token)
       localStorage.setItem("expirationDate", json.expirationDate)
       setToken(json.token)
+      setImgSrc(null)
     }
   }
 
@@ -37,30 +42,55 @@ function App() {
       () => {
         console.log({ expirationDate });
         const unixTime = Math.floor(Date.now() / 1000);
-        console.log({unixTime})
+        console.log({ unixTime })
         if (expirationDate && expirationDate < unixTime) {
-          console.log("Token has expired");
-          setToken(null);
-          setExpirationDate(null);
-          localStorage.clear();
+          clearToken()
         }
       }, 1000 * 60);
 
     return () => clearInterval(callback);
   }, [expirationDate, setExpirationDate, setToken]);
 
+  const clearToken = () => {
+    setToken(null);
+    setExpirationDate(null);
+    localStorage.clear();
+  }
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+    console.log(imageSrc)
+  }, [webcamRef, setImgSrc]);
+
   return (
     <div className="App">
-      <form onSubmit={submitFile}>
-        <label htmlFor="file">Select a file:</label>
-        <input type="file" id="file" name="file" />
-        <button type="submit">Send</button>
-      </form>
-
-      <div className="tokenDisplay">
+      {!imgSrc && !token &&
+        <><Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          videoConstraints={{ facingMode: 'user' }}
+        />
+          <button onClick={capture}>Capture photo</button>
+        </>
+      }
+      {imgSrc && !token && (
+        <>
+          <img
+            src={imgSrc}
+            alt="captured camera input"
+          />
+          <button onClick={() => setImgSrc(null)}>Discard</button>
+          <button onClick={() => submitImage(imgSrc)}>Send</button>
+        </>
+      )}
+      <div>
         {token}
       </div>
-      {token && <QRCodeSVG value={token} />}
+      {token && <><QRCodeSVG value={token} style={{ padding: "20px" }} />
+        <button onClick={clearToken}>Refresh</button>
+      </>}
     </div>
   );
 }
